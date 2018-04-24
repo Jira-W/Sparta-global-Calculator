635a1347d7316d1fb0c30a04e694ba3b4ccb62d5
Tech-A64 <tech-a64@Tech-A64s-MacBook-Air.local>
2018-04-24 10:43:07 +0100

    Homework

--- calculator.js
+++ calculator.js
@@ -1,56 +1,70 @@
-
-
 function addition() {
   var value1 = document.getElementById('firstnum').value;
   var value2 = document.getElementById('secnum').value;
 
-   var x = +value1 + +value2;
-    document.getElementById("result").innerHTML = x;
-    console.log(x);
+  var x = +value1 + +value2;
+  document.getElementById("result").innerHTML = x;
+  console.log(x);
 }
+
 function subs() {
   var value1 = document.getElementById('firstnum').value;
   var value2 = document.getElementById('secnum').value;
 
-   var x = +value1 - +value2;
-    document.getElementById("result").innerHTML = x;
-    console.log(x);
+  var x = +value1 - +value2;
+  document.getElementById("result").innerHTML = x;
+  console.log(x);
 }
+
 function multi() {
   var value1 = document.getElementById('firstnum').value;
   var value2 = document.getElementById('secnum').value;
 
-   var x = value1 * value2;
-    document.getElementById("result").innerHTML = x;
-    console.log(x);
+  var x = value1 * value2;
+  document.getElementById("result").innerHTML = x;
+  console.log(x);
 }
+
 function divide() {
   var value1 = document.getElementById('firstnum').value;
   var value2 = document.getElementById('secnum').value;
 
-   var x = value1 / value2;
-    document.getElementById("result").innerHTML = x;
-    console.log(x);
+  var x = value1 / value2;
+  document.getElementById("result").innerHTML = x;
+  console.log(x);
 }
 
 function BMI() {
   var Height = document.getElementById('bmi1').value;
   var Weight = document.getElementById('bmi2').value;
 
-  var x = Weight / (Height*Height);
-   document.getElementById("resultbmi").innerHTML = x;
-   console.log(x);
+  var x = Weight / (Height * Height);
+  document.getElementById("resultbmi").innerHTML = x;
+  console.log(x);
+
+  if (x >= 25) {
+    alert("Hit the Gym Bro!!");
+  }
+}
+function BMI2() {
+  var Height = document.getElementById('bmi1').value;
+  var Weight = document.getElementById('bmi2').value;
+
+  var x = (Weight*703) / (Height * Height);
+  document.getElementById("resultbmi").innerHTML = x;
+  console.log(x);
+
 }
 
 function changeUnit(y) {
   console.log(y.value);
   var unit = y.value;
-  if (unit == "Imperial"){
-    document.getElementById('imp1').innerHTML = "Height (in)"
-    document.getElementById('imp2').innerHTML = "Weight (lb)"
-  }
-  else {
-    document.getElementById('imp1').innerHTML = "Height (m)"
-    document.getElementById('imp2').innerHTML = "Weight (kg)"
+  if (unit == "Imperial") {
+    document.getElementById('imp1').innerHTML = "Height (in)";
+    document.getElementById('imp2').innerHTML = "Weight (lb)";
+    document.getElementById('bmiCal').onclick = BMI2();
+  } else {
+    document.getElementById('imp1').innerHTML = "Height (m)";
+    document.getElementById('imp2').innerHTML = "Weight (kg)";
   }
 }
